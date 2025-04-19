f2bdbf26f11cc26c2b289ead9d4a98a7e65da8a2
Arurken <134217531+Arurken@users.noreply.github.com>
2025-04-19 13:39:00 +0300

    Delete industrial_project directory

--- industrial_project/src/main/java/com/example/org/repositories/ProjectDocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.org.repositories;
-
-import com.example.org.entities.ProjectDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectDocumentRepository extends JpaRepository<ProjectDocument, Long> {
-}
